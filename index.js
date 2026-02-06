41b9514d05ea3195f2c2034034691f2bcb7d1690
adhamjon mansurov <stargamer9667@gmail.com>
2026-02-06 16:44:08 +0500

    arrays

---
+++ index.js
@@ -0,0 +1,94 @@
+// arr1
+// function getlevel(n) {
+//     let res = []
+//     for (let i = 1; i <= n; i++){
+//         res.push(2**i)
+//     }
+//     return res
+// }
+// console.log(getlevel(5));
+
+
+// arr3
+// let arr = [1,2,3,4,5]
+// arr.reverse()
+// console.log(arr);
+
+// arr4
+// let arr4 = [4,5,6,7,8,9]
+// let sanovchi = 0
+// for (let i = 0; i < arr4.length; i++) {
+//     if (arr4[i] % 2 != 0 ) {
+//         console.log(arr4[i]);   
+//         sanovchi++
+//     } 
+// }
+// console.log('toq sonlarning soni',sanovchi);
+
+// arr5
+// let arr5 = [4,5,6,7,8,9]
+// let res = []
+// for (let i = 0; i < arr5.length; i++) {
+//     if (arr5[i] % 2 == 0) {
+//         res.push(arr5[i])
+//     }
+// }
+// for (let i = arr5.length-1; i > 0; i--) {
+//     if (arr5[i] % 2 != 0) {
+//         res.push(arr5[i])
+//     }    
+// }
+// console.log(res.join(' '));
+
+// arr6
+// let n = [1,2,3,4,5,6,7]
+// let a = []
+// for (let i = 0; i < n.length; i+=2) {
+//     a.push(n[i])
+// }
+// console.log(a);
+
+// arr7
+// let n = [1,2,3,4,5,6,7]
+// let a = []
+// for (let i = n.length-1; i >= 0; i-=2) {
+//     a.push(n[i])
+// }
+// console.log(a);
+
+// arr8
+// let arr8 = [2,3,4,5,6,7,8,9,10,11]
+// let res = []
+// for (let i = 0; i < arr8.length; i+=2) {
+//     res.push(arr8[i])
+// }
+// for (let i = 1; i < arr8.length; i+=2) {
+//     res.push(arr8[i])
+// }
+// console.log(res);
+
+// arr 9 
+// let arr9 = [2,3,4,5,6,7,8,9,10]  
+// let toq = []
+// let juft = []
+
+// for (let i = 1; i < arr9.length; i += 2) {  // bu yerda toq sonlar indexi
+//     toq.push(arr9[i])
+// }
+// for (let i = arr9.length-1; i >= 0; i-=2) { // bu yerda juft sonlar indexi
+//     juft.push(arr9[i])    
+// }
+// console.log(`
+// toq indexlar  ${toq}
+// juft indexlar ${juft}`);
+
+// arr 10
+let arr10 = [1,2,3,4,5,6,7,8]
+res = []
+for (let index = 0; index < array.length; index++) {
+}
+
+
+
+
+
